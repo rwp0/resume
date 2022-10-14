05d0d46b68aef760fedce949e5d0106727e00528
Elvin Aslanov <rwp.primary@gmail.com>
2022-10-14 13:01:48 +0400

    Run, cPanel, HP

--- bin/resume.latex.pl
+++ bin/resume.latex.pl
@@ -1,19 +1,26 @@
-use v5.30;
-use Data::Dumper;
-use File::Spec;
-use constant
-{
-source => File::Spec -> catfile("$ENV{HOME}", qw(var git resume resume.tex)),
-output => File::Spec -> catfile("$ENV{HOME}", qw(var git resume out resume.pdf)),
-};
-use LaTeX::Driver; # cpan
+use v5.36;
+use Data::Printer;
+use Path::Tiny;
+use LaTeX::Driver;
+
+my $source = path( '/home/regular/IdeaProjects/resume/resume.tex' );
+my $output = path( '/home/regular/IdeaProjects/resume/out/resume.pdf' );
+
 my $latex = LaTeX::Driver -> new(
-  source  => source,
-  output  => output,
-  format  => 'pdf',
+  source => $source -> stringify, # can accept reference to scalar as text content rather than file name
+  output => $output -> stringify,
+  format => 'pdf',
 );
+
 $latex -> run_bibtex();
+
 my $run = $latex -> run();
-say Dumper $run, $latex -> stats();
-exec("firefox @{[output]} &") if $run;
+
+p $run, $latex -> stats();
+
+exec( "google-chrome-stable $output &" ) if $run;
+
 # shell: latex resume && bibtex resume && latex resume && latex resume && pdflatex resume && firefox resume.pdf
+
+# Debian:
+# texlive-xetex
